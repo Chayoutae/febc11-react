7aef6e4b1074a325442332f27410a5da2db5f541
chayoutae <cdx8722@gmail.com>
2024-11-12 16:07:42 +0900

    11-12

---
+++ workspace/ch02-start/vite/src/pages/todo/Todo.jsx
@@ -0,0 +1,12 @@
+import Title from "@Components/Title";
+
+function Todo(){
+    return(
+        <div>
+            <Title />
+        <p>Todo 목록</p>
+        </div>
+    );
+}
+
+export default Todo;
