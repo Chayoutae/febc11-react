2acca0c532407af3df656d94a556e213781c60e2
chayoutae <cdx8722@gmail.com>
2024-11-18 14:27:29 +0900

    11-18일

--- workspace/ch02-start/state/04/src/components/EditAddress.jsx
+++ workspace/ch02-start/state/04/src/components/EditAddress.jsx
@@ -1,19 +1,22 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
 
 export default function EditAddress({ addressBook, handleAddressChange }){
-    const list = addressBook.map(address => {
-        return(
-            <Fragment key={ address.id}>
-                <label htmlFor={ address.id}></label>
-                <input 
-                    id={ address.id }
-                    type="text"
-                    name={ address.name }
-                    value={ address.value }
-                    onChange={ handleAddressChange }
-                />
-            </Fragment>
-        );
-    });
-    return list;
+  const list = addressBook.map(address => {
+    return (
+      <Fragment key={ address.id }>
+        <label htmlFor={ address.id }>{ address.name }</label>
+        <input
+          id={ address.id }
+          type="text"
+          name={ address.name }
+          value={ address.value }
+          onChange={ handleAddressChange }
+        />
+        <br/>
+      </Fragment>
+    );
+  });
+
+  return list;
 }
+
