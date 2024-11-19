39dd976fd7ca9159669a560365691f25c20a3f1b
chayoutae <cdx8722@gmail.com>
2024-11-19 11:38:52 +0900

    11-19 2

--- workspace/app/todolist/01-container/src/pages/TodoContainer.jsx
+++ workspace/app/todolist/01-container/src/pages/TodoContainer.jsx
@@ -12,18 +12,18 @@ function TodoContainer(){
 
   const [itemList, setItemList] = useState(sampleItemList);
 
-  //할일 추가
+  // 할일 추가
   const addItem = (item) => {
-    //데이터 갱신(상태 변경)
+    // 데이터 갱신(상태 변경)
     const newItemList = [...itemList, item]; // 객체일 경우 새로운 객체로 만들어야 화면 갱신이 됨
     setItemList(newItemList); // setter를 이용해야 화면 갱신이 됨
   };
 
-  //할일 완료/미완료 처리
+  // 할일 완료/미완료 처리
   const toggleDone = (_id) => {
-    //데이터 갱신(상태 변경)
+    // 데이터 갱신(상태 변경)
     const newItemList = produce(itemList, (draft) => {
-      const item = draft.find( item => item._id === _id);
+      const item = draft.find(item => item._id === _id);
       item.done = !item.done;
     });
 
@@ -33,16 +33,17 @@ function TodoContainer(){
     console.log('새로운 itemList', newItemList);
   };
 
-  //할일 삭제
+  // 할일 삭제
   const deleteItem = (_id) => {
-    //데이터 갱신(상태 변경)
+    // 데이터 갱신(상태 변경)
     const newItemList = itemList.filter(item => item._id !== _id);
     setItemList(newItemList);
   };
 
-  return(
+  return (
     <Todo itemList={ itemList } addItem={ addItem } toggleDone={ toggleDone } deleteItem={ deleteItem } />
   );
+
 }
 
 export default TodoContainer;
