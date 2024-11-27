338dd835a0ebe937635d3d320765f9d1b3574650
chayoutae <cdx8722@gmail.com>
2024-11-27 09:09:23 +0900

    11-26일

--- workspace/app/todolist/05-api/src/pages/TodoDetail.jsx
+++ workspace/app/todolist/05-api/src/pages/TodoDetail.jsx
@@ -1,16 +1,18 @@
+import useAxios from "@hooks/useAxios";
+import useAxiosInstance from "@hooks/useAxiosInstance";
 import { useEffect, useState } from "react";
 import { Link, Outlet, useParams } from "react-router-dom";
 
-const dummyData = {
-  item: {
-    "_id": 5,
-    "title": "Javascript 공부",
-    "content": "열심히 하자",
-    "done": false,
-    "createdAt": "2024.11.21 16:49:00",
-    "updatedAt": "2024.11.21 16:49:00"
-  }
-};
+// const dummyData = {
+//   item: {
+//     "_id": 5,
+//     "title": "Javascript 공부",
+//     "content": "열심히 하자",
+//     "done": false,
+//     "createdAt": "2024.11.21 16:49:00",
+//     "updatedAt": "2024.11.21 16:49:00"
+//   }
+// };
 
 function TodoDetail() {
   // URL의 파라미터 추출
@@ -19,14 +21,24 @@ function TodoDetail() {
   const { _id } = useParams();
   console.log(_id);
 
-  const [data, setData] =useState();
+  const [data, setData] = useState();
+  // useEffect(() => {
+  //   // TODO: API 서버 통신
+    
+  //   setData(dummyData);
+  // }, []);
 
+  // const { data } = useAxios({ url: `/todolist/${_id}` });
+
+  const axios = useAxiosInstance();
+  // API 서버에서 상세정보를 조회
+  const fetchDetail = async () => {
+    const res = await axios.get(`/todolist/${_id}`);
+    setData(res.data);
+  };
   useEffect(() => {
-    // TODO: API 서버 통신
-
-    setData(dummyData);
-  }, []);
-
+    fetchDetail();
+  }, []); // 마운트될 때 한번만 호출
 
   return (
     <div id="main">
@@ -35,23 +47,21 @@ function TodoDetail() {
       { data && (
         <>
           <div className="todo">
+            
+            <div>제목 : { data.item.title }</div>
+            <div>내용 : { data.item.content }</div>
+            <div>상태 : { data.item.done ? '완료' : '미완료' }</div>
+            <div>작성일 : { data.item.createdAt }</div>
+            <div>수정일 : { data.item.updatedAt }</div>
 
-          <div>제목 : { data.item.title }</div>
-          <div>내용 : { data.item.content }</div>
-          <div>상태 : { data.item.done ? '완료' : '미완료' }</div>
-          <div>작성일 : { data.item.createdAt }</div>
-          <div>수정일 : { data.item.updatedAt }</div>
-
-          <Link to="./edit">수정</Link>
-          {/* <Link to={`/list/${_id}/edit`}>수정</Link> */}
-          <Link to="/list">목록</Link>
+            <Link to="./edit">수정</Link>
+            <Link to="/list">목록</Link>
           </div>
 
-        <Outlet context={{ item: data?.item }} />
+          <Outlet context={{ item: data.item, refetch: fetchDetail }} />
         </>
       ) }
 
-      
     </div>
   );
 }
