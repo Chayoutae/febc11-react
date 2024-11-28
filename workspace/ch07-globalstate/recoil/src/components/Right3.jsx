c0d9780ce636529d458fd9ac5c8c1a24ff6c8582
chayoutae <cdx8722@gmail.com>
2024-11-28 17:49:48 +0900

    11-28일

---
+++ workspace/ch07-globalstate/recoil/src/components/Right3.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { counterState } from '@recoil/atoms';
+
+function Right3() {
+  useEffect(()=>{
+    console.log('      # Right3 렌더링.');
+  });
+
+  const [count, setCount] = useRecoilState(counterState);
+
+  const countUp = (step) =>{
+    setCount(count + step);
+  };
+
+  return (
+    <div>
+      <h3>Right3</h3>
+      <button onClick={ () => countUp(1) }>+1</button>
+    </div>
+  );
+}
+
+export default Right3;
