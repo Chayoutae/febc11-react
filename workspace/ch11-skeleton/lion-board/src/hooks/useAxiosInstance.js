36fbdc53e7c7e477b005906148a33eecd800b74e
chayoutae <cdx8722@gmail.com>
2024-12-09 17:41:59 +0900

    12-09일

--- workspace/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
+++ workspace/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
@@ -13,6 +13,7 @@ function useAxiosInstance() {
 
   // 요청 인터셉터 추가하기
   instance.interceptors.request.use((config) => {
+    config.headers['Authorization'] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjI0LCJ0eXBlIjoidXNlciIsIm5hbWUiOiLrp7nqtawiLCJlbWFpbCI6Im1hbmdAbTkuY29tIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzM3MjYyMjEsImV4cCI6MTczMzgxMjYyMSwiaXNzIjoiRkVTUCJ9.mc4HC7pwIB6XQr2knW9QHfD7DgGvAtybccSplMzMpuc`
     // 요청이 전달되기 전에 필요한 공통 작업 수행
     config.params = {
       delay: 500,
